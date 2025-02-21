5c73bb341786bc3f3cd8ebdf8508663a6ea4bf24
Umberto Tramontano <u.tramontano92@libero.it>
2025-02-21 10:01:48 +0100

    Initial commit

---
+++ src/main/java/it/epicode/final_project_s7_l5/FinalProjectS7L5Application.java
@@ -0,0 +1,13 @@
+package it.epicode.final_project_s7_l5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectS7L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectS7L5Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/final_project_s7_l5/FinalProjectS7L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.final_project_s7_l5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectS7L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
